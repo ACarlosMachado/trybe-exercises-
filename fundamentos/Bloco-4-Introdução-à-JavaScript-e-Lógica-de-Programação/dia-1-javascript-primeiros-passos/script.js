c9ec671b5e44b285968d23a6bf42cc208eea3224
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-11-06 23:12:58 -0300

    Exercicios 3, 4, 5 e 6

--- fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-javascript-primeiros-passos/script.js
@@ -30,3 +30,76 @@ if (c > d){
     console.log(d);
 }
 
+// 3)Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+let e = 10;
+let f = 15;
+let g = 20;
+
+if ( e > f && e > g ){
+    console.log(e);
+}else if ( f > e && f > g){
+    console.log(f);
+}else{
+    console.log(g);
+}
+
+// 4)Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let h = 15;
+
+if (h > 0){
+    console.log("positive");
+}else if ( h < 0){
+    console.log("negative");
+}else{
+    console.log("zero");
+}
+
+// 5)Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+let angulo1 = 45;
+let angulo2 = -45;
+let angulo3 = 90;
+let somaAngulo = angulo1 + angulo2  + angulo3
+
+if (somaAngulo === 180){
+    console.log("true");
+}else if (somaAngulo !== 180 && (angulo1 > 0 && angulo2 > 0 && angulo3 > 0)){
+    console.log("false");
+}else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
+    console.log("Erro!")
+}
+
+// 6)Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
+// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+// Exemplo: bishop (bispo) -> diagonals (diagonais)
+
+let piece = "REi"
+let pieceLowCase = piece.toLocaleLowerCase();
+
+switch (pieceLowCase){
+    case "rei":
+        console.log("Rei: Uma casa em qualquer direção");
+        break;
+    case "Dama":
+        console.log("Dama: Quantas casas quiser, em qualquer direção.");
+        break;
+    case "torre":
+        console.log("Torre: Horizontal e vertical, quantas casas quiser.");
+        break;
+    case "bispo":
+        console.log("Bispo: Diagonal, quantas casas quiser, desde que da mesma cor do inicio do jogo.");
+        break;
+    case "cavalo":
+        console.log("Cavalo: Duas casa em uma direção + uma casa em 90º.");
+        break;
+    case "peão":
+        console.log("Peão: Um movimento a frente.");
+        break;
+    default:
+        console.log("Peça invalida: erro!")
+}
+
