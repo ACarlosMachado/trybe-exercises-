0c4aaaa10357a9a92f0fcf7fbe1814224646545f
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-11-08 19:08:41 -0300

    Correçao exercicio 1

--- fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for/script.js
@@ -1,17 +1,19 @@
 // 1)Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-//console.log(numbers);
-
-// 2)Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let soma = 0; //Cria uma variável soma e atribui a ela o valor 0
-for (let i = 0; i < numbers.length ; i+=1) { //Laço for, percorre todos os números do array numbers
-    soma += numbers[i]  // soma = soma + numbers[i]
-
+for (let i = 0; i < numbers.length; i+=1) {
+    console.log(numbers[i]);
 }
-    console.log(soma); //Retorna o resultado
+
+// 2)Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//let soma = 0; //Cria uma variável soma e atribui a ela o valor 0
+//for (let i = 0; i < numbers.length ; i+=1) { //Laço for, percorre todos os números do array numbers
+ //   soma += numbers[i]  // soma = soma + numbers[i]
+
+// }
+ //   console.log(soma); //Retorna o resultado
 
 
 
