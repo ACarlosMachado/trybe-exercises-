f4bc6bf5ad9831420cd2b82d60b9fc70f26190a1
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-11-08 19:02:07 -0300

    Exercicio 2

--- fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for/script.js
@@ -1,5 +1,18 @@
 // 1)Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//console.log(numbers);
+
+// 2)Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(numbers)
+let soma = 0; //Cria uma variável soma e atribui a ela o valor 0
+for (let i = 0; i < numbers.length ; i+=1) { //Laço for, percorre todos os números do array numbers
+    soma += numbers[i]  // soma = soma + numbers[i]
+
+}
+    console.log(soma); //Retorna o resultado
+
+
+
 
