c5e41ecfa9d7b4f628bbad07733823b6f21bdcb3
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-11-09 12:10:25 -0300

    exercicio 8

--- fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for/script.js
@@ -74,17 +74,23 @@
 //  }
 
 // 7) Utilizando for , descubra qual o menor valor contido no array e imprima-o;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//Resolução 7:
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let menor = numbers[1];
-for (let i =0; i < numbers.length ; i+=1){
-    if (numbers[i] < menor){
-        menor = numbers[i];
-    }
-}console.log(menor)
-
+// let menor = numbers[1];
+// for (let i =0; i < numbers.length ; i+=1){
+//     if (numbers[i] < menor){
+//         menor = numbers[i];
+//     }
+// }console.log(menor);
 
 // 8)Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+let umA25 = [];
+for (let i = 1; i <= 25 ; i+=1){
+    umA25 +=i
+    umA25 += " ";
+}
+console.log(umA25);
 // 9) Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
 
