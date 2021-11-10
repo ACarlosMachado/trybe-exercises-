a33a7a99de225b530a189e44a695d0314db16c50
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-11-10 12:00:42 -0300

    Nova resoluçao para o exercicio 2

--- fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
+++ fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
@@ -35,8 +35,18 @@ let asteriscos = '*'
 
 for (let index = 0 ; index < n ; index+=1){
     linha+=asteriscos;
-    for(let coluna = 0 ; coluna < index; coluna+=1 ){
+    for(let coluna = 0 ; coluna < n; coluna+=1 ){
     }    
     console.log(linha);
 }
+//Outra resolução:
+let size = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  
+  inputLine = inputLine + symbol;
+  console.log(inputLine);
+};
 
