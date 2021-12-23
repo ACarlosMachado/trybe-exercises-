0b9f74cbaee0007202901c24f3afc14b26a800c1
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-12-23 20:05:46 -0300

    Ex 6 - Parte III

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script-III.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script-III.js
@@ -65,3 +65,17 @@ function value(obj) {
 
 const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 console.log(allLessons);
+
+// 6) Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const getNumberOfStudents = (obj) => {
+    let total = 0;
+    const array = Object.keys(obj);
+    for (index in array) {
+      total += obj[array[index]].numeroEstudantes;
+    }
+    return total;
+  };
+  console.log(getNumberOfStudents(allLessons));
+
+  // 7) Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
