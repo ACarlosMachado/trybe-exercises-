a09c0b37a1915d15406a0bd1dbea38364adc84d2
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2021-12-23 16:16:05 -0300

    Ex 2 - Parte III

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script-III.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script-III.js
@@ -17,14 +17,18 @@ const lesson1 = {
     professor: 'Maria Clara',
     turno: 'noite',
   };
+// 1)Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
 function sessionNight (obj, keys, value){
     obj[keys] = value
 }sessionNight(lesson2, 'turno', 'noite')
-console.log(lesson2)
 
-// 1)Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 // 2)Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+function keys(obj) {
+Object.keys(obj)
+}keys(lesson3)
+
 // 3)Crie uma função para mostrar o tamanho de um objeto.
 // 4)Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 // 5)Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
