2ce85cae16c33869d9eacc4bbb1343ad9ce1807e
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-01-03 18:19:13 -0300

    Exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exersice5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exersice5.js
@@ -1,5 +1,4 @@
-// 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
-// Dica: use as funções filter e sort
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
 
 const books = [
     {
@@ -67,32 +66,14 @@ const books = [
   // Adicione o código do exercício aqui:
 
 //   const expectedResult = [
-//     {
-//       id: 6,
-//       name: 'O Chamado de Cthulhu',
-//       genre: 'Terror',
-//       author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-//       releaseYear: 1928,
-//     },
-//     {
-//       id: 3,
-//       name: 'Fundação',
-//       genre: 'Ficção Científica',
-//       author: { name: 'Isaac Asimov', birthYear: 1920 },
-//       releaseYear: 1951,
-//     },
-//     {
-//       id: 2,
-//       name: 'O Senhor dos Anéis',
-//       genre: 'Fantasia',
-//       author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-//       releaseYear: 1954,
-//     },
+//     'Frank Herbert',
+//     'George R. R. Martin',
+//     'Isaac Asimov',
+//     'J. R. R. Tolkien',
 //   ];
   
-  function oldBooksOrdered() {
+  function fantasyOrScienceFictionAuthors() {
     // escreva seu código aqui
-    return books.filter((book) => book.releaseYear > 60)
-    .sort((obj1, obj2) => obj1.releaseYear - obj2.releaseYear);
-    sort
-  }console.log(oldBooksOrdered());
+    return books.filter((book)=> (book.genre === 'Ficção Científica' || book.genre === 'Fantasia'))
+    .map((book) => book.author.name).sort()
+  } console.log(fantasyOrScienceFictionAuthors())
